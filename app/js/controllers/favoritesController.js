6eb5f220f0316972f7cb115b7e81b0f829189178
TChristal <thomaschristal@gmail.com>
2017-04-06 11:57:58 +0200

    some changes

--- app/js/controllers/favoritesController.js
+++ app/js/controllers/favoritesController.js
@@ -14,7 +14,6 @@ angular.module('app')
         CommentService.getAllByBeerId(beerId).then(function(res) {
           $scope.comments = res.data;
         }, function(err) {
-          console.error('err comments beer', err);
         });
       };
       $scope.delBeer = function (beerId) {
