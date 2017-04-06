9b50c04db6efc9108c199a96a4877d5a8a2ec620
TChristal <thomaschristal@gmail.com>
2017-04-06 18:18:09 +0200

    modif footer + another changes

--- app/js/controllers/searchController.js
+++ app/js/controllers/searchController.js
@@ -1,13 +1,17 @@
 angular.module('app')
     .controller('SearchController', function($scope, $http, CurrentUser, UserService, BeerService,  CommentService) {
       $scope.query = '';
-      $scope.addBeer = function () {
+      $scope.searchBeer = function () {
         $http.get("https://api.punkapi.com/v2/beers?beer_name="+ $scope.query).then(
           function (res) {
             $scope.beer = res.data;
             getComments(res.data[0].id);
           }, function error (err) {
           });
+      };
+      $scope.addBeer = function (beerId) {
+          UserService.addBeer(CurrentUser.user()._id, beerId).then(function(res) {
+        });
       };
       $scope.displayBlock = false;
       $scope.comments = [];
